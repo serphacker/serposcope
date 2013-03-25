298f06d8c09824ad3f4aad4f841a3c314adf1d38
serphacker <pierz@hotmail.it>
2013-03-25 14:11:25 +0100

    bulk import

--- proxies.php
+++ proxies.php
@@ -34,8 +34,7 @@ if (!empty($_POST)) {
         $linecount = 0;
         foreach ($lines as $line) {
             ++$linecount;
-            $line = trim($line);
-
+            $line = ltrim($line); //only ltrim
             // check if the line is not empty
             if (!empty($line)) {
 
@@ -44,12 +43,13 @@ if (!empty($_POST)) {
 
                 if (empty($proxy_line[0])) { // line with single :
                     $err = "bulkimport error line $linecount : each line should have at least an ip address";
+                    break;
                 }
 
                 $final[] = array(
                     'type' => $_POST['bulkimport-type'], // sanity check done later
                     'ip' => $proxy_line[0],
-                    'port' => isset($proxy_line[1]) ? $proxy_line[1] : null,
+                    'port' => isset($proxy_line[1]) ? intval($proxy_line[1]) : null,
                     'user' => isset($proxy_line[2]) ? $proxy_line[2] : null,
                     'password' => isset($proxy_line[3]) ? $proxy_line[3] : null,
                 );
@@ -59,62 +59,63 @@ if (!empty($_POST)) {
         $final[] = array(
             'type' => isset($_POST['type']) ? $_POST['type'] : null,
             'ip' => isset($_POST['ip']) ? $_POST['ip'] : null,
-            'port' => isset($_POST['port']) ? $_POST['port'] : null,
+            'port' => isset($_POST['port']) ? intval($_POST['port']) : null,
             'user' => isset($_POST['user']) ? $_POST['user'] : null,
             'password' => isset($_POST['password']) ? $_POST['password'] : null
         );
     }
 
-    // user sent nothing
-    if (empty($final)) {
-        $err = "bulkimport error:  each line should have at least an ip address";
-    } else {
+    if ($err==null) {
+        if (empty($final)) {
+            $err = "bulkimport error:  each line should have at least an ip address";
+        } else {
 
-        $q = "INSERT INTO `" . SQL_PREFIX . "proxy`(type,ip,port,user,password) VALUES ";
-        foreach ($final as $proxy) {
+            $q = "INSERT INTO `" . SQL_PREFIX . "proxy`(type,ip,port,user,password) VALUES ";
+            foreach ($final as $proxy) {
 
-            // we want to add them 1 by 1
-            if (!isset($proxy['ip'])) {
-                $err = "ip is mandatory";
-            } else {
+                // we want to add them 1 by 1
+                if (!isset($proxy['ip'])) {
+                    $err = "ip is mandatory";
+                } else {
 
-                switch ($proxy['type']) {
-                    case "http":
-                    case "socks":
-                        if (!isset($proxy['port']) || !is_numeric($proxy['port'])) {
-                            $err = "invalid port <strong>".h8(isset($proxy['port']) ? $proxy['port'] : "")."</strong> for <strong>".h8($proxy['ip'])."</strong>";
-                        }
+                    switch ($proxy['type']) {
+                        case "http":
+                        case "socks":
+                            if (!isset($proxy['port']) || !is_numeric($proxy['port'])) {
+                                $err = "invalid port <strong>" . h8(isset($proxy['port']) ? $proxy['port'] : "") . "</strong> for <strong>" . h8($proxy['ip']) . "</strong>";
+                            }
+                            break;
+
+                        case "iface":
+                            if (!empty($proxy['port']) || !empty($proxy['user']) || !empty($proxy['password'])) {
+                                $err = "iface proxy doesn't need port/username or password";
+                            }
+                            break;
+
+                        default:
+                            $err = "invalid proxy type";
+                    }
+
+                    if ($err != null) {
                         break;
+                    }
 
-                    case "iface":
-                        if (!empty($proxy['port']) || !empty($proxy['user']) || !empty($proxy['password'])) {
-                            $err = "iface proxy doesn't need port/username or password";
-                        }
-                        break;
-
-                    default:
-                        $err = "invalid proxy type";
+                    $q.="\n(";
+                    $q.="'" . addslashes($proxy['type']) . "',";
+                    $q.="'" . addslashes($proxy['ip']) . "',";
+                    $q.= (empty($proxy['port']) ? "NULL" : intval($proxy['port'])) . ",";
+                    $q.= (empty($proxy['user']) ? "NULL" : "'" . addslashes($proxy['user']) . "'") . ",";
+                    $q.= (empty($proxy['password']) ? "NULL" : "'" . addslashes($proxy['password']) . "'");
+                    $q.="),";
                 }
-
-                if ($err != null) {
-                    break;
-                }
-
-                $q.="\n(";
-                $q.="'" . addslashes($proxy['type']) . "',";
-                $q.="'" . addslashes($proxy['ip']) . "',";
-                $q.= (empty($proxy['port']) ? "NULL" : intval($proxy['port'])) . ",";
-                $q.= (empty($proxy['user']) ? "NULL" : "'" . addslashes($proxy['user']) . "'") . ",";
-                $q.= (empty($proxy['password']) ? "NULL" : "'" . addslashes($proxy['password']) . "'");
-                $q.="),";
             }
-        }
-        
-        //save in database if no error
-        if ($err == null) {
-            $q = trim($q, ",");
-            if (!$db->query($q)) {
-                $err = "sql error $q";
+
+            //save in database if no error
+            if ($err == null) {
+                $q = trim($q, ",");
+                if (!$db->query($q)) {
+                    $err = "sql error $q";
+                }
             }
         }
     }
@@ -129,81 +130,82 @@ if ($err != null) {
 ?>
 <script>
     bAllSelected = false;
-        $(function() {
+    $(function() {
 
-            $('#selectallproxy').click(function() {
+        $('#selectallproxy').click(function() {
 
             var selected = $('[name="idproxy[]"]');
-                if (bAllSelected) {
-            selected.attr('checked', false);
-                } else {
-            selected.attr('checked', true);
+            if (bAllSelected) {
+                selected.attr('checked', false);
+            } else {
+                selected.attr('checked', true);
             }
-        bAllSelected = !bAllSelected;
+            bAllSelected = !bAllSelected;
         });
 
-            $('#btndeleteproxy').click(function() {
+        $('#btndeleteproxy').click(function() {
             var selected = $('[name="idproxy[]"]:checked');
-                if (selected.length === 0) {
+            if (selected.length === 0) {
                 alert("no proxy selected");
-            return;
+                return;
             }
 
             var proxiesId = "";
-                selected.each(function(i, item) {
-            proxiesId += "&id[]=" + item.value;
+            selected.each(function(i, item) {
+                proxiesId += "&id[]=" + item.value;
             });
 
-                $.ajax({
+            $.ajax({
                 type: "POST",
                 url: "ajax.php",
-            data: "action=deleteproxy" + proxiesId
-                }).done(function(rawdata) {
+                data: "action=deleteproxy" + proxiesId
+            }).done(function(rawdata) {
                 data = JSON.parse(rawdata);
-                    if (data !== null) {
-                        if (data.deleted === true) {
-                    $('[name="idproxy[]"]:checked').parent().parent().remove();
-                        } else {
-                    alert("Can't delete proxies");
-             
-                    }                 } else {
-                alert("unknow error [1]");
-            }
-        });
+                if (data !== null) {
+                    if (data.deleted === true) {
+                        $('[name="idproxy[]"]:checked').parent().parent().remove();
+                    } else {
+                        alert("Can't delete proxies");
 
-        });
-
-            $('#btncheckproxy').click(function() {
-            var selected = $('[name="idproxy[]"]:checked');
-                if (selected.length === 0) {
-                alert("no proxy selected");
-            return;
-            }
-
-                selected.each(function(i, item) {
-                    $.ajax({
-                    type: "POST",
-                    url: "ajax.php",
-                data: "action=checkproxy&id=" + item.value
-                    }).done(function(rawdata) {
-                    data = JSON.parse(rawdata);
-                        if (data !== null) {
-                            if (data.result === "ok") {
-                            $('#status' + item.value).html("OK");
-                        $('#status' + item.value).css("color", "green");
-                            } else {
-                            $('#status' + item.value).html("ERR");
-                        $('#status' + item.value).css("color", "red");
                     }
-                        } else {
+                } else {
                     alert("unknow error [1]");
                 }
             });
+
         });
 
+        $('#btncheckproxy').click(function() {
+            var selected = $('[name="idproxy[]"]:checked');
+            if (selected.length === 0) {
+                alert("no proxy selected");
+                return;
+            }
+
+            selected.each(function(i, item) {
+                $.ajax({
+                    type: "POST",
+                    url: "ajax.php",
+                    data: "action=checkproxy&id=" + item.value
+                }).done(function(rawdata) {
+                    data = JSON.parse(rawdata);
+                    if (data !== null) {
+                        if (data.result === "ok") {
+                            $('#status' + item.value).html("OK");
+                            $('#status' + item.value).css("color", "green");
+                        } else {
+                            $('#status' + item.value).html("ERR");
+                            $('#status' + item.value).css("color", "red");
+                        }
+                    } else {
+                        alert("unknow error [1]");
+                    }
+                });
+            });
 
 
-    });
+
+        });
 
     });
 </script>
