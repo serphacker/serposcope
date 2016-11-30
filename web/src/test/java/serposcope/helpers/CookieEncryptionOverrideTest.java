405b82356421d23a4239a072bcd7ddaadcda8f7e
Pierre N <noguespi@gmail.com>
2016-11-30 14:41:20 +0100

    fix unit test for AES128 (limited encryption mode)

--- web/src/test/java/serposcope/helpers/CookieEncryptionOverrideTest.java
+++ web/src/test/java/serposcope/helpers/CookieEncryptionOverrideTest.java
@@ -12,7 +12,6 @@ import static ninja.utils.CookieEncryption.ALGORITHM;
 import ninja.utils.NinjaConstant;
 import ninja.utils.NinjaMode;
 import ninja.utils.NinjaPropertiesImpl;
-import ninja.utils.SecretGenerator;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -21,40 +20,41 @@ import static org.junit.Assert.*;
  * @author admin
  */
 public class CookieEncryptionOverrideTest {
-    
+
     public CookieEncryptionOverrideTest() {
     }
 
     @Test
     public void testSomeMethod() throws Exception {
-        
+
         String secret1 = "4MeHInvL49wEH4DVmD9DHYwCCTaRl7u7r886FgIoGG5CAe92lN972JNxeUhMXVln";
         String secret2 = "Vso90TgeMWCdw0pqSXxcI4iTqERnoiicKNM14l0oKU6xHERBRhIPOYxM4QxfEVgo";
-        
+
         NinjaPropertiesImpl props = new NinjaPropertiesImpl(NinjaMode.prod);
         props.setProperty(NinjaConstant.applicationSecret, secret1);
         props.setProperty(NinjaConstant.applicationCookieEncrypted, "false");
-        
+
         CookieEncryptionOverride cookie = new CookieEncryptionOverride(props);
-        
+
         assertEquals("abcdef", cookie.encrypt("abcdef"));
         props.setProperty(NinjaConstant.applicationCookieEncrypted, "true");
         cookie.update(props);
-        assertEquals("3ue3IUUnTHfT2wvATBQiqg", cookie.encrypt("abcdef"));
-        
+        if (Cipher.getMaxAllowedKeyLength(ALGORITHM) == 128) {
+            assertEquals("isBWgLfMoK5wwjSI5uqKgQ", cookie.encrypt("abcdef"));
+        } else {
+            assertEquals("3ue3IUUnTHfT2wvATBQiqg", cookie.encrypt("abcdef"));
+        }
+
         CookieEncryptionOverride cookie2 = new CookieEncryptionOverride(props);
         assertEquals(cookie.encrypt("abcdef"), cookie2.encrypt("abcdef"));
-        
+
         assertEquals(cookie.fieldSecretKeySpec.get(cookie), cookie2.fieldSecretKeySpec.get(cookie2));
-        
+
         props.setProperty(NinjaConstant.applicationSecret, secret2);
         cookie.update(props);
-        assertNotEquals(cookie.fieldSecretKeySpec.get(cookie), cookie2.fieldSecretKeySpec.get(cookie2));        
+        assertNotEquals(cookie.fieldSecretKeySpec.get(cookie), cookie2.fieldSecretKeySpec.get(cookie2));
         assertNotEquals(cookie.encrypt("abcdef"), cookie2.encrypt("abcdef"));
-        
+
     }
-    
-    
-    
-    
+
 }
