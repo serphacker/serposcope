15190b153e66110359aed0e0f97d1b10b435a822
Pierre N <noguespi@gmail.com>
2017-03-16 09:40:37 +0100

    increase max retry to 6

--- scraper/src/main/java/com/serphacker/serposcope/scraper/google/scraper/GoogleScraper.java
+++ scraper/src/main/java/com/serphacker/serposcope/scraper/google/scraper/GoogleScraper.java
@@ -48,7 +48,7 @@ import org.slf4j.LoggerFactory;
  */
 public class GoogleScraper {
     
-    public final static int MAX_RETRY = 3;
+    public final static int MAX_RETRY = 6;
     
     final static BasicClientCookie NCR_COOKIE = new BasicClientCookie("PREF", "ID=1111111111111111:CR=2");
     static {
