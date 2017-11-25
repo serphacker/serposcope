8280d0d4584f0a56027f02ba078faf7aa03c7107
Pierre N <noguespi@gmail.com>
2017-11-25 14:56:24 +0100

    dbc recaptcha sitekey support

--- scraper/src/test/java/com/serphacker/serposcope/scraper/captcha/solver/DeathByCaptchaSolverIT.java
+++ scraper/src/test/java/com/serphacker/serposcope/scraper/captcha/solver/DeathByCaptchaSolverIT.java
@@ -11,6 +11,7 @@ import com.google.common.io.ByteStreams;
 import com.serphacker.serposcope.scraper.DeepIntegrationTest;
 import com.serphacker.serposcope.scraper.captcha.Captcha;
 import com.serphacker.serposcope.scraper.captcha.CaptchaImage;
+import com.serphacker.serposcope.scraper.captcha.CaptchaRecaptcha;
 import com.serphacker.serposcope.scraper.http.ScrapClient;
 import java.io.IOException;
 import org.junit.Test;
@@ -25,23 +26,23 @@ import static org.junit.Assert.assertTrue;
  * @author admin
  */
 public class DeathByCaptchaSolverIT extends DeepIntegrationTest {
-    
+
     public DeathByCaptchaSolverIT() {
     }
-    
+
     String dbcLogin;
     String dbcPassword;
-    
+
     @Before
     public void readCredentials() throws Exception {
         dbcLogin = props.getProperty("dbclogin");
-        dbcPassword = props.getProperty("dbcpassword");        
+        dbcPassword = props.getProperty("dbcpassword");
         assertNotNull(dbcLogin);
         assertNotNull(dbcPassword);
     }
-    
+
     @Test
-    public void testGetUserData(){
+    public void testGetUserData() {
         DeathByCaptchaSolver solver = new DeathByCaptchaSolver(dbcLogin, dbcPassword);
         System.out.println(solver.getUserData());
     }
@@ -51,19 +52,19 @@ public class DeathByCaptchaSolverIT extends DeepIntegrationTest {
         DeathByCaptchaSolver solver = new DeathByCaptchaSolver(dbcLogin, dbcPassword);
         assertNotEquals(0, solver.getCredit());
     }
-    
+
     @Test
-    public void testHasCredit(){
+    public void testHasCredit() {
         DeathByCaptchaSolver solver = new DeathByCaptchaSolver(dbcLogin, dbcPassword);
         assertTrue(solver.hasCredit());
     }
-    
+
     @Test
-    public void testTestLogin(){
+    public void testTestLogin() {
         DeathByCaptchaSolver solver = new DeathByCaptchaSolver(dbcLogin, dbcPassword);
-        assertTrue(solver.testLogin());        
+        assertTrue(solver.testLogin());
     }
-    
+
     @Test
     public void testSuccessSolve() throws IOException {
         DeathByCaptchaSolver solver = new DeathByCaptchaSolver(dbcLogin, dbcPassword);
@@ -74,10 +75,10 @@ public class DeathByCaptchaSolverIT extends DeepIntegrationTest {
         assertEquals(Captcha.Error.SUCCESS, captcha.getError());
         assertEquals("hyped", captcha.getResponse());
     }
-    
+
     @Test
     public void testSolveRandom() throws Exception {
-        try(ScrapClient cli = new ScrapClient()){
+        try (ScrapClient cli = new ScrapClient()) {
             cli.get("https://sorry.google.com/sorry/image?id=15854950164179955873");
             CaptchaImage captcha = new CaptchaImage(new byte[][]{cli.getContent()});
             DeathByCaptchaSolver solver = new DeathByCaptchaSolver(dbcLogin, dbcPassword);
@@ -87,12 +88,26 @@ public class DeathByCaptchaSolverIT extends DeepIntegrationTest {
             System.out.println("duration : " + captcha.getSolveDuration());
         }
     }
-    
+
     @Test
     public void testExtractId() throws Exception {
         DeathByCaptchaSolver solver = new DeathByCaptchaSolver(dbcLogin, dbcPassword);
         assertEquals("1477215", solver.extractId("http://api.dbcapi.me/api/captcha/1477215"));
     }
-    
-    
+
+    @Test
+    public void testSolverRecaptcha() throws Exception {
+
+        try (DeathByCaptchaSolver solver = new DeathByCaptchaSolver(dbcLogin, dbcPassword);) {
+            CaptchaRecaptcha captcha = new CaptchaRecaptcha("6LfydQgUAAAAAMuh1gRreQdKjAop7eGmi6TrNIzp", "https://anti-captcha.com/recaptcha");
+            solver.setTimeoutMS(120000);
+            solver.solve(captcha);
+            System.out.println("id : " + captcha.getId());
+            System.out.println("response : " + captcha.getResponse());
+            System.out.println("duration : " + captcha.getSolveDuration());
+            System.out.println("error : " + captcha.getError());
+        }
+
+    }
+
 }
